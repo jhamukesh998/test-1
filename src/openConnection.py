b040bd467638a66b5af9eba329be18df10b9ac88
Damnik Jain <jaindamnik203@gmail.com>
2023-04-13 12:06:26 -0700

    added network scanner

--- src/openConnection.py
+++ src/openConnection.py
@@ -1,89 +1,7 @@
-# import socket
-
-# # Get the private IP address of the host machine
-# HOST = socket.gethostbyname(socket.gethostname())
-
-# PORT = 2222  # Port number you want to open
-
-# # Create a new socket object
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     # Set the socket options to allow reuse of the address and port
-#     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-#     # Bind the socket to the host and port
-#     s.bind((HOST, PORT))
-
-#     # Listen for incoming connections
-#     s.listen(1)
-
-#     # Print a message indicating that the server is listening
-#     print(f"Server listening on {HOST}:{PORT}...")
-
-#     # Accept incoming connections and handle them
-#     while True:
-#         # Wait for a connection
-#         conn, addr = s.accept()
-
-#         # Print a message indicating that a connection was received
-#         print(f"Connection received from {addr}")
-
-#         # Send a response to the client
-#         message = "Hello, client!"
-#         conn.sendall(message.encode())
-
-#         # Close the connection
-#         conn.close()
-
-
-
-
-
-
-
-
-# import socket
-
-# # Get the private IP address of the host machine
-# HOST = socket.gethostbyname(socket.gethostname())
-
-# PORT = 2222  # Port number you want to open
-
-# # Create a new socket object
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     # Set the socket options to allow reuse of the address and port
-#     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-#     # Bind the socket to the host and port
-#     s.bind((HOST, PORT))
-
-#     # Listen for incoming connections
-#     s.listen(1)
-
-#     # Print a message indicating that the server is listening
-#     print(f"Server listening on {HOST}:{PORT}...")
-
-#     # Accept incoming connections and handle them
-#     while True:
-#         # Wait for a connection
-#         conn, addr = s.accept()
-
-#         # Print a message indicating that a connection was received
-#         print(f"Connection received from {addr}")
-
-#         # Receive incoming messages from the client and send a response
-#         while True:
-#             data = conn.recv(1024)
-#             if not data:
-#                 break
-#             message = f"Received your message: {data.decode()}"
-#             conn.sendall(message.encode())
-#             print(f"Sent message: {message}")
-
-#         # Close the connection
-#         conn.close()
-
+#C:\Users\jaind\Desktop\Projects\Assignments\DISML\test-1\src
 
 import socket
+import time
 
 # Get the private IP address of the host machine
 def get_private_ip():
@@ -116,6 +34,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     # Set the socket options to allow reuse of the address and port
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #s.settimeout(30) 
 
     # Bind the socket to the host and port
     s.bind((HOST, PORT))
@@ -126,7 +45,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # Print a message indicating that the server is listening
     print(f"Server listening on {HOST}:{PORT}...")
 
-
     # Accept incoming connections and handle them
     while True:
         # Wait for a connection
@@ -134,10 +52,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         # Print a message indicating that a connection was received
         print(f"Connection received from {addr}")
+        print(type(addr))
 
-        # Send a response to the client
-        message = "Hello, client!"
-        conn.sendall(message.encode())
+        print(addr[0])
+
+        # # Send a response to the client
+        # message = "Hello, client!"
+        # conn.sendall(message.encode())
+
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sf:
+            print("sleeping and then will connect!")
+            time.sleep(3)
+            sf.settimeout(0.1)
+            sf.connect((addr[0], PORT))
+            sf.sendall(b"Hello, network!")
+            response = sf.recv(1024)
+            print(f"Response from {target}: {response.decode()}")
+
 
         # Close the connection
         conn.close()
