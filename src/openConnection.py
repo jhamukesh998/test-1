0cae0a8453d79f24fe9a4c7cbd48991437425f1b
jhamukesh998 <mukesh.jha@spit.ac.in>
2023-04-13 00:20:53 -0700

    Adds connection open

---
+++ src/openConnection.py
@@ -0,0 +1,35 @@
+import socket
+
+# Get the private IP address of the host machine
+HOST = socket.gethostbyname(socket.gethostname())
+
+PORT = 2222  # Port number you want to open
+
+# Create a new socket object
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Set the socket options to allow reuse of the address and port
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Bind the socket to the host and port
+    s.bind((HOST, PORT))
+
+    # Listen for incoming connections
+    s.listen(1)
+
+    # Print a message indicating that the server is listening
+    print(f"Server listening on {HOST}:{PORT}...")
+
+    # Accept incoming connections and handle them
+    while True:
+        # Wait for a connection
+        conn, addr = s.accept()
+
+        # Print a message indicating that a connection was received
+        print(f"Connection received from {addr}")
+
+        # Send a response to the client
+        message = "Hello, client!"
+        conn.sendall(message.encode())
+
+        # Close the connection
+        conn.close()
